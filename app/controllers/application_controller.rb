4fc10a39aeaa8f56691e532d5fed9f70e8cdfba3
JP Slavinsky <jpslav@gmail.com>
2011-02-08 08:20:22 -0600

    added devise views, fixed deprecated error messages, added name info to user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   
   #before_filter :login_required
   
+  before_filter :authenticate_user!
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
